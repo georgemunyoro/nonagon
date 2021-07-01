18df31526cb3db60f0126cfed3239638f3806555
George Munyoro <george.guva@outlook.com>
2021-07-02 01:23:50 +0200

    refactor: change Radio component to RadioList feat: build RadioList component

--- src/components/Radio/Radio.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./Radio.css";
-
-const Radio = ({}) => {
-  return <div className="Radio"></div>
-};
-
-export default Radio;
---
+++ src/components/RadioList/RadioList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import "./RadioList.css";
+
+const RadioList = ({ data, onSelect, horizontal }) => {
+  const name = uuidv4();
+
+  return <div className={`RadioList ${horizontal && "horizontal"}`}>
+    {
+      data.map(({ label, value }) => {
+        const id = uuidv4();
+        return <div>
+          <input onClick={() => onSelect(value)} className="RadioItem" type="radio" {...{ id, value, name }} />
+          <label for={id}>{label}</label>
+        </div>
+      })
+    }
+  </div>
+};
+
+export default RadioList;
