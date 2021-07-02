e36847b74ef410c92b3ec1cbdea9a5224f0a180d
George Munyoro <george.guva@outlook.com>
2021-07-02 17:58:42 +0200

    generate divider component

---
+++ src/components/Divider/Divider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Divider.css";
+
+const Divider = ({}) => {
+  return <div className="Divider"></div>
+};
+
+export default Divider;
---
+++ src/stories/Divider.stories.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Divider from "../components/Divider";
+
+export default {
+  title: "Components/Divider",
+  component: Divider,
+  argtypes: {},
+};
+
+const Template = (args) => <Divider {...args} />;
+
+export const divider = Template.bind({});
+
+divider.args = {};
