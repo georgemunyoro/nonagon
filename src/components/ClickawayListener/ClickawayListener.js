18b5d7664ba5c73b3a0be3f2560944f8694a1bc5
George Munyoro <george.guva@outlook.com>
2021-07-07 19:20:32 +0200

    generate clickaway listener component

---
+++ src/components/ClickawayListener/ClickawayListener.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./ClickawayListener.css";
+
+const ClickawayListener = ({}) => {
+  return <div className="ClickawayListener"></div>
+};
+
+export default ClickawayListener;
---
+++ src/stories/ClickawayListener.stories.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import ClickawayListener from "../components/ClickawayListener";
+
+export default {
+  title: "Components/ClickawayListener",
+  component: ClickawayListener,
+  argtypes: {},
+};
+
+const Template = (args) => <ClickawayListener {...args} />;
+
+export const clickawaylistener = Template.bind({});
+
+clickawaylistener.args = {};
