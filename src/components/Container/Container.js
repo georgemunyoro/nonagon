8883215dab2033234bd62b8519158de1d6a93797
George Munyoro <george.guva@outlook.com>
2021-07-07 21:36:09 +0200

    feat: add classes prop to container component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./Container.css";
 
-const Container = ({ children, style, flex }) => {
-  return <div style={style} className={`Container ${flex && "flex"}`}>{children}</div>;
+const Container = ({ children, style, flex, classes = []}) => {
+  return <div style={style} className={`Container ${flex && "flex"} ${classes.join("")}`}>{children}</div>;
 };
 
 export default Container;
