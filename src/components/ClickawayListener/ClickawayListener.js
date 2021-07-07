9e4c34f7d1ba2136606026f66492a310df11544b
George Munyoro <george.guva@outlook.com>
2021-07-07 21:35:33 +0200

    feat: implement Clickaway Listener component

--- src/components/ClickawayListener/ClickawayListener.js
+++ src/components/ClickawayListener/ClickawayListener.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import "./ClickawayListener.css";
 
-const ClickawayListener = ({}) => {
-  return <div className="ClickawayListener"></div>
+const ClickawayListener = ({ children, onClickAway }) => {
+  const id = uuidv4();
+
+  const onClickHandler = (event) => {
+    if (event.target.classList[0] === id) {
+      onClickAway();
+    }
+  };
+
+  return (
+    <div className={`${id} ClickawayListener`} onClick={onClickHandler}>
+      {children}
+    </div>
+  );
 };
 
 export default ClickawayListener;
