955cb1080244a1f9f5b762af8d2be0afc27b78ba
George Munyoro <george.guva@outlook.com>
2021-07-07 16:36:27 +0200

    build: add index file to components folder for easier importing of components

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Button from "./Button";
+import Box from "./Box";
+import ButtonGroup from "./ButtonGroup";
+import Checkbox from "./Checkbox";
+import Container from "./Container";
+import Divider from "./Divider";
+import Drawer from "./Drawer";
+import Hidden from "./Hidden";
+import List from "./List";
+import ListItem from "./ListItem";
+import RadioList from "./RadioList";
+import Switch from "./Switch";
+import Text from "./Text";
+import TextField from "./TextField";
+
+export {
+  Button,
+  Box,
+  ButtonGroup,
+  Checkbox,
+  Container,
+  Divider,
+  Drawer,
+  Hidden,
+  List,
+  ListItem,
+  RadioList,
+  Switch,
+  Text,
+  TextField,
+};
