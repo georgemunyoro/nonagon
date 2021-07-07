811f3f71f778426f0aa600283e1839a8fa002570
George Munyoro <george.guva@outlook.com>
2021-07-07 21:40:52 +0200

    feat: implement drawer component

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -2,12 +2,22 @@ import "./Drawer.css";
 
 import React from "react";
 import Container from "../Container";
+import ClickawayListener from "../ClickawayListener";
+import Hidden from "../Hidden";
 
-const Drawer = ({ position, isOpen }) => {
+const Drawer = ({ position, isOpen, children, onClose }) => {
+  const onCloseHandler = () => {
+    onClose();
+  };
+
   return (
-    <Container
-      classes={[`Drawer ${position} ${isOpen && "isOpen"}`]}
-    ></Container>
+    <Hidden hidden={!isOpen}>
+      <ClickawayListener onClickAway={onCloseHandler}>
+        <Container classes={[`Drawer ${position} ${isOpen && "isOpen"}`]}>
+          <Container classes={[`DrawerContainer`]}>{children}</Container>
+        </Container>
+      </ClickawayListener>
+    </Hidden>
   );
 };
 
